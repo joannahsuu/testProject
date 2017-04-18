a05a4c00f8773254ee2a94e2246f36e68cd2166b
PC219-13\NTUT <joannahsuu@gmail.com>
2017-04-18 10:11:10 +0800

    20170418修改

---
+++ src/exer02/paint.java
@@ -0,0 +1,24 @@
+package exer02;
+import java.util.*;
+public class paint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		final int COVERAGE = 300;
+		int sq_feet = 0;
+		int cans;
+		
+		
+		
+		
+		System.out.println("Enter number of square feet to be painted:");
+		
+		while(scan.hasNext()){
+			if(scan.hasNextInt()) {
+				}
+			}
+		
+	}
+
+}
