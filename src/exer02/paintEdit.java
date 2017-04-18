21fa1210c79e33080e9e495b6ef3a09dab826aeb
PC219-13\NTUT <joannahsuu@gmail.com>
2017-04-18 10:36:06 +0800

    edit again

--- src/exer02/paintEdit.java
+++ src/exer02/paintEdit.java
@@ -4,12 +4,12 @@ public class paintEdit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scan = new Scanner(System.in);
+//		Scanner scan = new Scanner(System.in);
 		final int COVERAGE = 300;
 		int sq_feet = 0;
 		int cans;
 		
-		
+
 		System.out.println("Enter number of square feet to be painted:");
 		
 //		while(scan.hasNext()){
